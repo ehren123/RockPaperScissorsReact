232af8140c23df7f2aed2564ac698ba884551024
Ehren Lillies <ehren.lillies@nexusinno.com>
2023-04-17 00:46:02 -0400

    added controllers

---
+++ Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RockPaperScissors.Controllers
+{
+    using Commands;
+    using Dtos;
+    using Services;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGame(CreateGameCommand command)
+        {
+            await _gameService.CreateGame(command);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Produces(typeof(UserDto))]
+        [Route("{name}")]
+        public async Task<IActionResult> GetUserByName([FromRoute] string name)
+        {
+            var user = await _gameService.GetUserByName(name);
+            return Ok(user);
+        }
+    }
+}
---
+++ Controllers/LeaderBoardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockPaperScissors.Services;
+
+namespace RockPaperScissors.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LeaderBoardController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public LeaderBoardController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLeaderBoard()
+        {
+            var leaderBoard = await _gameService.GetLeaderBoard();
+            return Ok(leaderBoard);
+        }
+    }
+}
