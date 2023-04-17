000f66c2c26ef199d19f6a4c345ef0b16b9a758c
Ehren Lillies <ehren.lillies@nexusinno.com>
2023-04-17 06:57:54 -0400

    added nav routes

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Nav from './Components/Nav/Nav';
+import Play from './Components/Play/Play';
+import Leaderboard from './Components/Leaderboard/Leaderboard';
+
 
 function App() {
   return (
     <>
       <div className="container-fluid">
         <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Click play to play or leaderboard to view the leaderboard</h1>} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
       </div>
     </>
   );
